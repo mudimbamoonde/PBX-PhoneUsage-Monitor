d46569a9ecedfb5c5a546a32eb56837b9d84c5a2
Mudimba Moonde <mudimbamoonde@gmail.com>
2020-11-27 16:30:47 +0200

    dbConnection

--- dbConnection.py
+++ dbConnection.py
@@ -6,7 +6,7 @@ class Master:
 
     # connect = pymssql.connect(server=connections["ServerName"],user=connections["user"],
     #                                 password=connections["password"],database=connections["database"])
-    def Connection(self,connect,connections[]):
+    def Connection(self,connect,connections):
         pass
 
     def GetData(self):
